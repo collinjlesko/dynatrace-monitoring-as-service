72dcb6ae26defcaded8b5538c246cccce5ad0aeb
Andreas Grabner <grabnerandi@gmx.at>
2018-07-26 12:26:58 +0200

    adding new launch configurations

--- frontend-app/run_docker.sh
+++ frontend-app/run_docker.sh
@@ -29,6 +29,15 @@ for (( i=1; i<=$NO_INSTANCES; i++))
 do
   INSTANCE_NAME=frontend-app-instance-$i
   echo "$INSTANCE_NAME:$BIND_PORT - BUILD: $BUILD_NUMBER"
-  sudo docker run --name $INSTANCE_NAME -p $BIND_PORT:80 -e BUILD_NUMBER=$BUILD_NUMBER -d frontend-app:latest
+
+  # RAW: This is the "plain" regular launch of a container - nothing specific to Dynatrace OneAgent
+  # sudo docker run --name $INSTANCE_NAME -p $BIND_PORT:80 -e BUILD_NUMBER=$BUILD_NUMBER -d frontend-app:latest
+
+  # Step 1: the following line is to ENABLE Dynatrace OneAgent Custom Tagging and Custom Meta Data Passing
+  sudo docker run --name $INSTANCE_NAME -p $BIND_PORT:80 -e BUILD_NUMBER=$BUILD_NUMBER -e DT_TAGS="BUILD_NUMBER=$BUILD_NUMBER SERVICE_TYPE=FRONTEND" -e DT_CUSTOM_PROP="BUILD_NUMBER=$BUILD_NUMBER BIND_PORT=$BIND_PORT" -d frontend-app:latest
+
+  # Step 2: the following line leverages DT_NODE_ID to differentiate each individual container instance as its own Process Group Instance
+  # sudo docker run --name $INSTANCE_NAME -p $BIND_PORT:80 -e BUILD_NUMBER=$BUILD_NUMBER -e DT_NODE_ID=$i -e DT_TAGS="BUILD_NUMBER=$BUILD_NUMBER SERVICE_TYPE=FRONTEND" -e DT_CUSTOM_PROP="BUILD_NUMBER=$BUILD_NUMBER BIND_PORT=$BIND_PORT" -d frontend-app:latest
+
   BIND_PORT=$(($BIND_PORT+1))
 done
--- run_frontend2builds_clustered.sh
+++ run_frontend2builds_clustered.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# will run 2 frontends with build 1
+# Will Run 4 Frontends - 2 with Build 1 and 2 with Build 2
 cd frontend-app
 bash run_docker.sh 2 8081 1
+bash run_docker.sh 2 8083 2
 
 cd ../backend-service
 bash run_docker.sh
 
 cd ../frontend-loadbalancer
-bash run_docker.sh 2
+bash run_docker.sh 4
